2c27d3e192274d07e47ef9a8377ea5c836c484ef
Subhadeep95 <subhadeep.info95@gmail.com>
2016-12-18 22:04:06 +0530

    Notice is fixed(Inserting values not done yet.)

--- app/src/main/java/com/example/subhadeep95/cs_8/Login.java
+++ app/src/main/java/com/example/subhadeep95/cs_8/Login.java
@@ -1,10 +1,11 @@
 package com.example.subhadeep95.cs_8;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,11 +14,22 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 
 public class Login extends AppCompatActivity {
 
     EditText emailText,passText;
+    String name;
 
     private FirebaseAuth mAuth;
 
@@ -29,28 +41,38 @@ public class Login extends AppCompatActivity {
         emailText = (EditText)findViewById(R.id.email);
         passText = (EditText)findViewById(R.id.password);
 
+        if(!loadData().equals("")) {
+            startActivity(new Intent(Login.this,classRoutine.class));
+            finish();
+        }
+        else
+        {
+            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        }
+
         mAuth = FirebaseAuth.getInstance();
+
     }
 
     public void signin(View view)
     {
-        String email = emailText.getText().toString();
-        String pass = passText.getText().toString();
+        final String email = emailText.getText().toString();
+        final String pass = passText.getText().toString();
 
-        if(!validateForm(email,pass)) {
-            return;
-        }
-        else
+        if(validateForm(email,pass))
         {
+            final ProgressDialog progressDialog = ProgressDialog.show(Login.this,"Signing You In...","Please Wait",false,false);
+
             mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
+                    progressDialog.dismiss();
                     if (!task.isSuccessful()) {
                         Toast.makeText(Login.this, "You are have either not registered or your internet is slow.", Toast.LENGTH_SHORT).show();
                     }
                     else {
                         Toast.makeText(Login.this,"Welcome to CS8",Toast.LENGTH_SHORT).show();
-                        //String name = fetchName();
+                        fetchName(email,pass);
                         startActivity(new Intent(Login.this,classRoutine.class));
                         finish();
                     }
@@ -85,4 +107,53 @@ public class Login extends AppCompatActivity {
         //startActivity(new Intent(Login.this,.class));
     }
 
+    protected String loadData() {
+        String FILENAME = "name.txt";
+        String out = "";
+
+        try {
+            FileInputStream fis1 = getApplication().openFileInput(FILENAME);
+            BufferedReader br1 = new BufferedReader(new InputStreamReader(fis1));
+            String sLine1;
+            while (((sLine1 = br1.readLine()) != null)) {
+                out += sLine1;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return out;
+    }
+
+    public void fetchName(final String email, final String pass)
+    {
+        String url = "https://cs-8-cc5a1.firebaseio.com/User/";
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReferenceFromUrl(url);
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot: dataSnapshot.getChildren())
+                {
+                    User_Structure user_structure = snapshot.getValue(User_Structure.class);
+                    if(user_structure.getEmail().equalsIgnoreCase(email) && user_structure.getPass().equalsIgnoreCase(pass)) {
+                        name = user_structure.getName();
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+        savedata(name);
+    }
+
+    public void savedata(String name)
+    {
+        String FILENAME = "name.txt";
+        try {
+            FileOutputStream fos = getApplication().openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            fos.write(name.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
